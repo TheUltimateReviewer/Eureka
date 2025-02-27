35495c86731c8c9e877cd1e135e46d5d6f30dd32
Cristian Hernandez <cristianmanuel2304@hotmail.com>
2025-02-27 10:25:19 -0500

    eureka

---
+++ src/test/java/Review/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package Review.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
